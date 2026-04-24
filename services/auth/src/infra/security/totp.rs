480ab708fe770f1b262f0f16218d98f9ba6ed7e9
bitquence <imbitquence@gmail.com>
2026-04-25 01:44:22 +0200

    make TotpMfa basic struct

--- services/auth/src/infra/security/totp.rs
+++ services/auth/src/infra/security/totp.rs
@@ -1,8 +1,7 @@
 use crate::domain::ports::mfa_service::{MfaSecret, MfaService};
 use rand::Rng;
 
-pub struct TotpMfa {
-}
+pub struct TotpMfa;
 
 impl MfaService for TotpMfa {
 	fn generate_mfa_secret(&self) -> MfaSecret {
