08f8c73b1be6f35364bd50826de114e977d4d166
bitquence <imbitquence@gmail.com>
2026-04-25 01:44:41 +0200

    ChaChaEncryption key field

--- services/auth/src/infra/security/encryption.rs
+++ services/auth/src/infra/security/encryption.rs
@@ -1,6 +1,16 @@
 use crate::domain::ports::encryption_service::{Ciphertext, EncryptionService};
 
-pub struct ChaChaEncryption;
+pub struct ChaChaEncryption {
+    key: Vec<u8>
+}
+
+impl ChaChaEncryption {
+    pub fn from_key(key: &[u8]) -> Self {
+        Self {
+            key: Vec::from(key)
+        }
+    }
+}
 
 impl EncryptionService for ChaChaEncryption {
     fn encrypt(&self, data: &[u8]) -> Ciphertext {
