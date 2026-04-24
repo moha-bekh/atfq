fee5f13db0ea49cbb937e39564ee710e2d7b6ba5
bitquence <imbitquence@gmail.com>
2026-04-25 00:53:41 +0200

    Ciphertext getters

--- services/auth/src/domain/ports/encryption_service.rs
+++ services/auth/src/domain/ports/encryption_service.rs
@@ -3,6 +3,16 @@ pub struct Ciphertext {
 	nonce: Vec<u8>
 }
 
+impl Ciphertext {
+	pub fn data(&self) -> &Vec<u8> {
+		&self.data
+	}
+
+	pub fn nonce(&self) -> &Vec<u8> {
+		&self.nonce
+	}
+}
+
 pub trait EncryptionService: Send + Sync {
 	fn encrypt(&self, data: &[u8]) -> Ciphertext;
 	fn decrypt(&self, data: &Ciphertext) -> Vec<u8>;
