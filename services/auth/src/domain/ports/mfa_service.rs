307a78c18bbe416033d4d7f626db588839d4bc71
bitquence <imbitquence@gmail.com>
2026-04-26 02:00:11 +0200

    MfaService::code_currently_valid

--- services/auth/src/domain/ports/mfa_service.rs
+++ services/auth/src/domain/ports/mfa_service.rs
@@ -1,9 +1,11 @@
 use crate::domain::ports::encryption_service::Ciphertext;
+use crate::domain::error::DomainError;
 
 pub type MfaSecret = [u8; 20];
 pub type EncryptedMfaSecret = Ciphertext;
 
 pub trait MfaService: Send + Sync {
 	fn generate_mfa_secret(&self) -> MfaSecret;
+	fn code_currently_valid(&self, secret: &MfaSecret, code: &str) -> Result<bool, DomainError>;
 	fn make_human_readable_secret(&self, secret: &MfaSecret) -> String;
 }
--- services/auth/src/infra/security/totp.rs
+++ services/auth/src/infra/security/totp.rs
@@ -1,5 +1,7 @@
 use crate::domain::ports::mfa_service::{MfaSecret, MfaService};
+use crate::domain::error::DomainError;
 use rand::Rng;
+use totp_rs::{Algorithm, TOTP};
 
 pub struct TotpMfa;
 
@@ -8,6 +10,16 @@ impl MfaService for TotpMfa {
 		let mut bytes: MfaSecret = MfaSecret::default();
 		rand::rng().fill_bytes(&mut bytes);
 		bytes
+	}
+
+	fn code_currently_valid(&self, secret: &MfaSecret, code: &str) -> Result<bool, DomainError> {
+		let totp = TOTP::new(Algorithm::SHA1, 6, 1, 30, secret.to_vec())
+			.map_err(|_| DomainError::Internal("OTP instantiation error".to_string()))?;
+
+		let valid = totp.check_current(code)
+			.map_err(|_| DomainError::Internal("System clock error".to_string()))?;
+
+		Ok(valid)
 	}
 
 	fn make_human_readable_secret(&self, secret: &MfaSecret) -> String {
