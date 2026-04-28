693d4ae115f2055c0eb10f9f4bfa41dc14b27095
moha-bekh <moha.bekh.pro@gmail.com>
2026-04-28 17:42:12 +0200

    redo user service in rust

---
+++ services/user/build.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let proto_file = "proto/user/user.proto";
+    
+    tonic_build::configure()
+        .build_server(true)
+        .file_descriptor_set_path(out_dir.join("user_descriptor.bin"))
+        .compile(&[proto_file], &["proto"])?;
+    Ok(())
+}
